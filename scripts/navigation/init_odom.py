2f5f35f3236c6da142136b7189b498c7cad47393
AndreiMitriakov <andrei.mitriakov@gmail.com>
2020-03-11 18:35:18 +0100

    some2

--- scripts/navigation/init_odom.py
+++ scripts/navigation/init_odom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import rospy
 import tf
@@ -6,7 +7,7 @@ import time
 
 
 '''
-It is not implemented.
+This node has to connect 3 separated tf trees: base_link, usb_cam, world. 
 '''
 
 
@@ -15,17 +16,23 @@ class InitOdom:
         rospy.init_node('init_odom_broadcaster')
         self.listener = tf.TransformListener()
         self.br = tf.TransformBroadcaster()
+        self.world_origin = []
+        self.first_launch = True
 
     def run(self):
         RATE = 10.0 # Hz
-        while not rospy.is_shutdown():
-            try:
-                (trans, rot) = self.listener.lookupTransform('/ar_marker_1', '/ar_marker_0', rospy.Time(0))
-                self.br.sendTransform(trans, rot, rospy.Time.now(), "map", "ar_marker_1")
-                time.sleep(1/RATE)
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                time.sleep(1/RATE)
-                continue
+        if self.first_launch:
+            (trans, rot) = self.listener.lookupTransform("/ar_marker_1", "/usb_cam", rospy.Time(0))
+            trans[2] = 0
+            self.world_origin.append(trans)
+            self.world_origin.append(rot)
+            self.first_launch = False
+        else:
+            while not rospy.is_shutdown():
+                # I. Publish marker_1 -> world transform;
+                # usb -> world tree
+                self.br.sendTransform(self.world_origin[0], self.world_origin[1], rospy.Time.now(),  "/ar_marker_1",
+                                      "/world")
 
 
 if __name__ == '__main__':
